01cbbd70fe0128ad55e001063b6e6a2180c7448d
lacarmen <carmen.la@mail.utoronto.ca>
2015-05-28 23:41:31 -0400

    Fixed cell generation

--- src/cljs_life/core.cljs
+++ src/cljs_life/core.cljs
@@ -2,17 +2,11 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
+(enable-console-print!)
+
 (def world-size 500)
 (def size 5)
 (def max-cells (inc (/ world-size size)))
-
-(defn setup []
-  (q/frame-rate 1)
-  (q/background 240)
-  (q/stroke 100)
-  (q/fill 100)
-  ; Initial state
-  (set (repeatedly 1200 #(vector (rand-int max-cells) (rand-int max-cells)))))
 
 (defn neighbours
   "Returns the coordinates of the cell's 8 neighbours"
@@ -24,15 +18,29 @@
 (defn will-live?
   "Checks if a cell will be alive in the next iteration"
   [world [cell count]]
-  (or (= 3 count) (and (= 2 count) (contains? world cell))))
+  (or (= 3 count) (and (= 2 count) (some #{cell} world))))
+
+(defn next-cells [world]
+  (->> (mapcat neighbours world)
+       frequencies
+       (filter #(will-live? world %))
+       distinct
+       (map first)))
+
+(defn setup []
+  (q/frame-rate 1)
+  (q/background 240)
+  (q/stroke 240)
+  (q/fill 180)
+  ; Initial state
+  (->> (repeatedly 1200 #(vector (rand-int max-cells) (rand-int max-cells)))
+       distinct
+       next-cells))
 
 (defn update-world
   "Updates the set of living cells"
   [world]
-  (->> (mapcat neighbours world)
-       frequencies
-       (filter #(will-live? world %))
-       set))
+  (next-cells world))
 
 (defn draw-world
   "Resets the background and draws the next set of cells"
